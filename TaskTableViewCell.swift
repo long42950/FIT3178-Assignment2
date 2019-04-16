38ca123277cd1755663cd3bcae6bf921aad0363e
long42950 <long42950@gmail.com>
2019-04-16 21:00:05 +1000

    E2P2 - Implement the Search Task Page

---
+++ TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  Assignment 2
+//
+//  Created by Chak Lee on 16/4/19.
+//  Copyright © 2019 Chak Lee. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
