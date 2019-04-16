11b4c92f52396f885881416602c9d7d7d9ad8d15
long42950 <long42950@gmail.com>
2019-04-16 18:18:02 +1000

    initial commit

---
+++ Assignment 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by Chak Lee on 16/4/19.
+//  Copyright © 2019 Chak Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  Assignment 2
+//
+//  Created by Chak Lee on 16/4/19.
+//  Copyright © 2019 Chak Lee. All rights reserved.
+//
+
+import Foundation
+
+class Task: NSObject {
+    var taskTitle: String?
+    var taskDescription: String?
+    var dueDate: String?
+    var isCompleted: Bool?
+}
