ffb6a1c0f5318fd64eac3c667988ddec7e494c6f
long42950 <long42950@gmail.com>
2019-04-17 15:49:01 +1000

    E2P3 - Create All Task List

---
+++ TaskStatusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskStatusTableViewCell.swift
+//  Assignment 2
+//
+//  Created by Chak Lee on 17/4/19.
+//  Copyright © 2019 Chak Lee. All rights reserved.
+//
+
+import UIKit
+
+class TaskStatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
